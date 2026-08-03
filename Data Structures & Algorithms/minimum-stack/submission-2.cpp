4a7848afab6a04623ebf53ec1b07dbed554d4bd4
Ishan Agarwal <121796309+Ishan0106@users.noreply.github.com>
2026-08-03 18:07:46 +0530

    Add: minimum-stack - submission-2

---
+++ Data Structures & Algorithms/minimum-stack/submission-2.cpp
@@ -0,0 +1,31 @@
+class MinStack {
+private:
+    std::stack<int> stack;
+    std::stack<int> minStack;
+
+public:
+    MinStack() {}
+
+    void push(int val) {
+        stack.push(val);
+        val = std::min(val, minStack.empty() ? val : minStack.top());
+        // this is the tricky part // please remember this please 
+        // please revise
+        // jaise jaise elemenet aa rhe hai , dusre stack me unke samne abhi konsa min hai
+        // vo rkhna
+        minStack.push(val);
+    }
+
+    void pop() {
+        stack.pop();
+        minStack.pop();
+    }
+
+    int top() {
+        return stack.top();
+    }
+
+    int getMin() {
+        return minStack.top();
+    }
+};
